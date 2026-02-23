62a72cdf0b6658ba04f0b1c67a69b75e8f45f44e
Inga Rondel <90716651+rondelinga@users.noreply.github.com>
2026-02-24 00:02:06 +0300

    feat: add username for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_23_202252) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_23_205952) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_23_202252) do
     t.integer "role", default: 0, null: false
     t.bigint "telegram_id"
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["telegram_id"], name: "index_users_on_telegram_id", unique: true
   end
 end
