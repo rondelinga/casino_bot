ebaf091ee4f03a1b0405e1c871e1d94c6772b8c5
rondelinga <90716651+rondelinga@users.noreply.github.com>
2026-02-24 00:08:18 +0300

    feat: add spins realization and user roles (#4)
    * feat: add spin logic to casino
    
    * feat: add roles
    
    * feat: add username for users

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  DAILY_TOKENS = 100
+
+  enum :role, { regular: 0, creator: 1, admin: 2 }
+
+  def display_name
+    username.present? ? "@#{username}" : telegram_id.to_s
+  end
+
+  def give_daily_tokens!
+    return if last_daily.present? && last_daily >= Date.today
+    update!(balance: balance.to_i + DAILY_TOKENS, last_daily: Date.today)
+  end
+
+  def self.find_or_create_by_telegram(telegram_id)
+    user = find_or_create_by(telegram_id: telegram_id)
+    user.give_daily_tokens!
+    user
+  end
+end
